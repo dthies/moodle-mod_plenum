9c277e17c9aa82387e1563f20538c342c0e5d1b3
Daniel Thies <dthies@gmail.com>
2025-08-20 14:46:46 -0500

    Bug fixes

--- version.php
+++ version.php
@@ -26,7 +26,7 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'mod_plenum';
 $plugin->release = '1.1.3';
-$plugin->version = 2025012806;
+$plugin->version = 2025012807;
 $plugin->requires = 2024041600;
 $plugin->maturity = MATURITY_STABLE;
 $plugin->dependencies = [
